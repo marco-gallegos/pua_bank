bab7a1a58dbc84f73be5c41ec45ca2796d732987
Marco A. Gallegos <ma_galeza@hotmail.com>
2025-11-28 01:12:03 -0600

    chore(expenses): added icons

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { createRoot } from 'react-dom/client'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+import 'bootstrap-icons/font/bootstrap-icons.min.css'
 import './index.css'
 import App from './App.tsx'
 
